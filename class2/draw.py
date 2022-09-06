f74c78f34ee3e185f1eee34887fb6440648ab10e
m200is <changhy27@naver.com>
2022-09-06 15:41:31 +0900

    second

---
+++ class2/draw.py
@@ -0,0 +1,29 @@
+import random
+import turtle as myTurtle
+myTurtle.colormode(255)
+
+t = myTurtle.Pen()
+t.pensize(2)
+t.shape("classic")
+t.speed(0)
+t.goto(0,0)
+r=255
+g=0
+b=0
+for i in range(1275):
+    t.color(r,g,b)
+    t.forward(100)
+    t.goto(0,0)
+    t.right(0.2823529411764706)
+    if r==255 and b==0 and g!=255:
+        g+=1
+    elif g==255 and b==0 and r>0:
+        r-=1
+    elif r==0 and g==255 and b<255:
+        b+=1
+    elif b==255 and r==0 and g>0:
+        g-=1
+    elif g==0 and b==255 and r<255:
+        r+=1
+
+myTurtle.exitonclick()
